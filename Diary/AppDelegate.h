919ae0a8379e2063bc5ef4be9c65dcf4a4189994
清水 翔太郎 <shimizushoutarou@shimizu-no-MacBook-Air.local>
2014-06-06 20:18:16 +0900

    Initial Commit

---
+++ Diary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Diary
+//
+//  Created by 清水 翔太郎 on 2014/06/06.
+//  Copyright (c) 2014年 清水 翔太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Diary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by 清水 翔太郎 on 2014/06/06.
+//  Copyright (c) 2014年 清水 翔太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
